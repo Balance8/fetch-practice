672ac53087fc0cd6e603e2029acbdd066890dc0d
Michael Slocum <mslocum819@gmail.com>
2018-05-21 15:15:22 -0700

    Main Code

---
+++ src/api/api.js
@@ -0,0 +1,10 @@
+const API = {
+  fetchPopularRepositories: function(language) {
+    let URI = `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`;
+    return fetch(URI, {
+      method: "GET"
+    }).then(response => response.json());
+  }
+};
+
+export default API;
